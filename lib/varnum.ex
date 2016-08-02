e4f5bd9abaaf30764edfbae966c609419dc30a0a
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-08-02 14:58:30 -0500

    Add price function.

--- lib/varnum.ex
+++ lib/varnum.ex
@@ -1,2 +1,25 @@
 defmodule Varnum do
+  @moduledoc"""
+  Manages Varnum features such as fetching price, saving data, and sending mail.
+  """
+
+  @sp500_url "https://www.google.com/finance?q=INDEXSP%3A.INX&sq=INDEXSP"
+
+  def get_price(url) do
+    HTTPoison.get(url)
+    |> handle_response
+  end
+
+  def get_price() do
+    get_price(@sp500_url)
+  end
+
+  def handle_response({:ok, response}) do
+    [{_, _, contents}] = Floki.find(response.body, ".pr span")
+    dirty_price = hd(contents)
+    price_value = String.replace(dirty_price, ",", "")
+    String.to_float(price_value)
+  end
+
+  def handle_response({:error, _}), do: 0
 end
--- test/varnum_test.exs
+++ test/varnum_test.exs
@@ -2,7 +2,11 @@ defmodule VarnumTest do
   use ExUnit.Case
   doctest Varnum
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "retrieves current price as float" do
+    assert is_float(Varnum.get_price())
+  end
+
+  test "bad URL returns zero price" do
+    assert Varnum.get_price("https://badurl.abcxyz") == 0
   end
 end
