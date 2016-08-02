dc6fb2b4a4794f7d4cf99554c0fc5f047dd6cd06
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-08-02 14:48:06 -0500

    Add dependencies and project info to mix configuration.

--- mix.exs
+++ mix.exs
@@ -3,6 +3,9 @@ defmodule Varnum.Mixfile do
 
   def project do
     [app: :varnum,
+     name: "Varnum",
+     source_url: "https://github.com/jga/varnum",
+     homepage_url: "https://github.com/jga/varnum",
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
@@ -14,7 +17,7 @@ defmodule Varnum.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +30,10 @@ defmodule Varnum.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:floki, "~> 0.9.0"},
+     {:httpoison, "~> 0.9"},
+     {:eiconv, github: "zotonic/eiconv"},
+     {:mailman, "~> 0.2.2"},
+     {:ex_doc, "~>0.12"}]
   end
 end
